c7a93186d9c0fecf947730ef2bafe565c7d42d8f
Pearl Anyanwu <peaanyanwu@gmail.com>
2024-03-28 16:56:09 -0700

    edited css for email input in desktop design

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let email = document.getElementById("email");
 const regex = /^[a-zA-Z0-9_]+@[a-zA-Z]+\.[a-z]{3}$/;
 
 submitButton.addEventListener("click", () => {
-  if (email.value == "" || !regex.test(email.value)) {
-    alert("Please enter a valid email.")
-  } else {
-    alert("Subscribed!");
-  }
-});
+  // if (email.value == "" || !regex.test(email.value)) {
+  //   alert("Please enter a valid email.")
+  // } else {
+  //   alert("Subscribed!");
+  // }
+
+  return email.value == "" || !regex.test(email.value) ? alert("Please enter a valid email") : alert("Subscribed!");
+})
